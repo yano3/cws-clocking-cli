f91308f37adde4b10133692b434731f651db9033
Teppei Yano <buckelwal@gmail.com>
2019-04-30 22:33:13 +0900

    clocking in/out

--- cli.go
+++ cli.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/Songmu/prompter"
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
 )
+
+const clockingTypeIn string = "syussya"
+const clockingTypeOut string = "taisya"
 
 // Exit codes are int values that represent an exit code for a particular error.
 const (
@@ -17,6 +32,58 @@ type CLI struct {
 	// outStream and errStream are the stdout and stderr
 	// to write message from the CLI.
 	outStream, errStream io.Writer
+}
+
+func clockIn(clockingOut bool) error {
+	cws_url := os.Getenv("CWS_URL")
+	cws_userid := os.Getenv("CWS_USERID")
+	cws_password := os.Getenv("CWS_PASSWORD")
+	clocking_endpoint := cws_url + "/srwtimerec"
+
+	var clockingType string
+	if clockingOut {
+		clockingType = clockingTypeOut
+	} else {
+		clockingType = clockingTypeIn
+	}
+
+	values := url.Values{}
+	values.Add("user_id", cws_userid)
+	values.Add("password", cws_password)
+	values.Add("dakoku", clockingType)
+
+	res, err := http.PostForm(clocking_endpoint, values)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	doc, _ := goquery.NewDocumentFromReader(res.Body)
+
+	if mes := doc.Find("table > tbody > tr > td > strong").Text(); mes != "\u00a0" {
+		errMes, _ := decodeShiftJIS(mes)
+		return errors.New(errMes)
+	}
+
+	clockingTime, err := decodeShiftJIS(doc.Find("table > tbody > tr > td > font > u").Text())
+	if err != nil {
+		return err
+	}
+
+	reg := regexp.MustCompile(`.+(\d\d)時(\d\d)分.+`)
+	clockingTime = reg.ReplaceAllString(clockingTime, "$1:$2")
+
+	fmt.Println(clockingTime)
+
+	return nil
+}
+
+func decodeShiftJIS(s string) (string, error) {
+	r, err := ioutil.ReadAll(transform.NewReader(strings.NewReader(s), japanese.ShiftJIS.NewDecoder()))
+	if err != nil {
+		return "", err
+	}
+	return string(r), nil
 }
 
 // Run invokes the CLI with the given arguments.
@@ -34,11 +101,11 @@ func (cli *CLI) Run(args []string) int {
 	flags.SetOutput(cli.errStream)
 
 	flags.BoolVar(&yes, "yes", false, "Skip y/n prompt")
-	flags.BoolVar(&yes, "y", false, "Skip y/n prompt(Short)")
+	flags.BoolVar(&yes, "y", false, "Skip y/n prompt (Short)")
 	flags.BoolVar(&out, "out", false, "Clocking out")
-	flags.BoolVar(&out, "o", false, "Clocking out(Short)")
+	flags.BoolVar(&out, "o", false, "Clocking out (Short)")
 	flags.BoolVar(&status, "status", false, "Just show clock in/out time and exit")
-	flags.BoolVar(&status, "s", false, "Just show clock in/out time and exit(Short)")
+	flags.BoolVar(&status, "s", false, "Just show clock in/out time and exit (Short)")
 
 	flags.BoolVar(&version, "version", false, "Print version information and quit.")
 
@@ -53,9 +120,16 @@ func (cli *CLI) Run(args []string) int {
 		return ExitCodeOK
 	}
 
-	_ = yes
+	if !yes && !prompter.YN("OK?", true) {
+		fmt.Println("Canceled")
+		return ExitCodeError
+	}
 
-	_ = out
+	err := clockIn(out)
+	if err != nil {
+		fmt.Println(err)
+		return ExitCodeError
+	}
 
 	_ = status
 
